6b6e61c65466079292be7bf80a272602a489a6c8
Chris Liu-Beers <tomatillodesign@users.noreply.github.com>
2020-04-03 12:47:45 -0400

    Update clb-custom-scripts.js

--- js/clb-custom-scripts.js
+++ js/clb-custom-scripts.js
@@ -40,3 +40,67 @@ var  entryContent = document.querySelector( '.entry-content' ),
 }
 
 setLinkTargets();
+
+
+
+
+
+
+
+
+// Fix for callouts that are purposely missing titles
+var  fixMissingCalloutTitles = function() {
+
+     var  calloutBoxes = document.querySelectorAll( '.clb-callout-area' ),
+          atts = calloutBoxes.attributes,
+          numBoxes = calloutBoxes.length;
+
+     for( var i = 0, max = numBoxes; i < max; i++ ) {
+
+          var calloutHeading = calloutBoxes[i].getElementsByTagName('h3');
+
+          if( calloutHeading[0].innerHTML === '' ) {
+               calloutHeading[0].style.display = 'none';
+          }
+
+     }
+
+}
+
+fixMissingCalloutTitles();
+
+
+
+
+
+// This function will check all links on the page, incl header + footer, for '#subscribe' and will add the modal data markup
+// Modal HTML markup should be added in clb-cpt.php, functions.php, etc.
+var  setSubscribeModal = function() {
+
+     //console.log('setSubscribeModal 12:10p');
+
+var  fullContent = document.querySelector( '.site-container' ),
+     atts = fullContent.attributes,
+     numLinks = fullContent.getElementsByTagName('a').length;
+
+     for( var i = 0, max = numLinks; i < max; i++ ) {
+
+          var  a = fullContent.getElementsByTagName('a')[i],
+               linkSite = a.origin,
+               link = a.getAttribute("href");
+
+               if( link === '#subscribe' ) {
+                    a.removeAttribute("target");
+                    //a.setAttribute("toggle", "modal");
+                    a.dataset.toggle = "modal";
+                    //data-toggle="modal"
+                    console.log( a );
+               }
+
+
+
+     }
+
+}
+
+setSubscribeModal();
